8fb2f8e376f78436316551d90d208d0574445918
rhpinotti <rhpinotti@gmail.com>
2024-05-25 10:47:37 -0300

    Animando o personagem

---
+++ player/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var is_running: bool = false
+
+func _process(delta: float):
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_running:
+			animation_player.play("idle")
+			is_running = false
+		else:
+			animation_player.play("run")
+			is_running = true
