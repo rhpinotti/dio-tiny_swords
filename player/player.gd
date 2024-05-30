fedd8114c0672d836a936612d3163fd7f0dc5aa5
rhpinotti <rhpinotti@gmail.com>
2024-05-30 11:21:06 -0300

    Organizando o código

--- player/player.gd
+++ player/player.gd
@@ -5,11 +5,31 @@ extends CharacterBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 
+var input_vector: Vector2 = Vector2(0,0)
 var is_running: bool = false
+var was_running: bool = false
 var is_attacking: bool = false
 var attack_cooldown: float = 0.0
 
 func _process(delta):
+	read_input()
+	update_attack_cooldown(delta)
+	play_run_idle_animation()
+	rotate_sprite()
+		
+	#ataque
+	if Input.is_action_just_pressed("attack"):
+		attack()
+
+func _physics_process(delta: float):
+	# modificar velocidade
+	var target_velocity = input_vector * speed * 100
+	if is_attacking:
+		target_velocity *= 0.25
+	velocity = lerp(velocity, target_velocity, 0.05)
+	move_and_slide()
+
+func update_attack_cooldown(delta):
 	if is_attacking:
 		attack_cooldown -= delta
 		if attack_cooldown <= 0:
@@ -17,9 +37,9 @@ func _process(delta):
 			is_running = false
 			animation_player.play("idle")
 
-func _physics_process(delta: float):
+func read_input():
 	#obter input vector
-	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	#apagar deadzone do input vector
 	var deadzone = 0.15
@@ -27,25 +47,20 @@ func _physics_process(delta: float):
 		input_vector.x = 0
 	if abs(input_vector.y) < deadzone:
 		input_vector.y = 0
-	
-	# modificar velocidade
-	var target_velocity = input_vector * speed * 100
-	if is_attacking:
-		target_velocity *= 0.25
-	velocity = lerp(velocity, target_velocity, 0.05)
-	move_and_slide()
-	
+		
 	# atualizar is_running
-	var was_running = is_running
+	was_running = is_running
 	is_running = not input_vector.is_zero_approx()
-	
+
+func play_run_idle_animation():
 	if !is_attacking:
 		if was_running != is_running:
 			if is_running:
 				animation_player.play("run")
 			else:
 				animation_player.play("idle")
-			
+
+func rotate_sprite():
 	#girar sprite
 	if input_vector.x > 0:
 		#desmarcar flip_h sprite2d
@@ -53,11 +68,7 @@ func _physics_process(delta: float):
 	elif input_vector.x < 0:
 		#marcar flip_h sprite2d
 		sprite.flip_h = true
-		
-	#ataque
-	if Input.is_action_just_pressed("attack"):
-		attack()
-		
+
 func attack():
 	if is_attacking:
 		return
