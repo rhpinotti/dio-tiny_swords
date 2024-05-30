97d79d1172c965077183ed4a12442559e377e241
rhpinotti <rhpinotti@gmail.com>
2024-05-30 10:58:14 -0300

    Programando o ataque

--- player/player.gd
+++ player/player.gd
@@ -6,6 +6,16 @@ extends CharacterBody2D
 @onready var sprite: Sprite2D = $Sprite2D
 
 var is_running: bool = false
+var is_attacking: bool = false
+var attack_cooldown: float = 0.0
+
+func _process(delta):
+	if is_attacking:
+		attack_cooldown -= delta
+		if attack_cooldown <= 0:
+			is_attacking = false
+			is_running = false
+			animation_player.play("idle")
 
 func _physics_process(delta: float):
 	#obter input vector
@@ -20,6 +30,8 @@ func _physics_process(delta: float):
 	
 	# modificar velocidade
 	var target_velocity = input_vector * speed * 100
+	if is_attacking:
+		target_velocity *= 0.25
 	velocity = lerp(velocity, target_velocity, 0.05)
 	move_and_slide()
 	
@@ -27,11 +39,12 @@ func _physics_process(delta: float):
 	var was_running = is_running
 	is_running = not input_vector.is_zero_approx()
 	
-	if was_running != is_running:
-		if is_running:
-			animation_player.play("run")
-		else:
-			animation_player.play("idle")
+	if !is_attacking:
+		if was_running != is_running:
+			if is_running:
+				animation_player.play("run")
+			else:
+				animation_player.play("idle")
 			
 	#girar sprite
 	if input_vector.x > 0:
@@ -40,3 +53,17 @@ func _physics_process(delta: float):
 	elif input_vector.x < 0:
 		#marcar flip_h sprite2d
 		sprite.flip_h = true
+		
+	#ataque
+	if Input.is_action_just_pressed("attack"):
+		attack()
+		
+func attack():
+	if is_attacking:
+		return
+		
+	#attack_side_1
+	#attack_side_2
+	animation_player.play("attack_side_1")
+	attack_cooldown = 0.6
+	is_attacking = true
