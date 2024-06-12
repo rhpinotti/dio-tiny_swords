9dfd6ce9e4aa31ee9353689ed0f9e347c51b1a5a
rhpinotti <rhpinotti@gmail.com>
2024-06-12 11:03:33 -0300

    Limites do mundo, drops de carne

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -2,8 +2,13 @@ class_name Enemy
 
 extends Node2D
 
+@export_category("Life")
 @export var health: int = 10
 @export var death_prefab: PackedScene
+@export_category("Drops")
+@export var drop_chance: float = 0.1
+@export var drop_items: Array[PackedScene]
+@export var drop_chances: Array[float]
 
 @onready var damage_digit_marker = $DamageDigitMarker
 
@@ -34,9 +39,39 @@ func damage(amount: int):
 		die()
 
 func die():
+	#caveira
 	if death_prefab:
 		var death_object = death_prefab.instantiate()
 		death_object.position = position
 		get_parent().add_child(death_object)
 	
+	#drop
+	if randf() <= drop_chance:
+		drop_item()
+	
 	queue_free()
+
+func drop_item():
+	var drop = get_random_drop_item().instantiate()
+	drop.position = position
+	get_parent().add_child(drop)
+
+func get_random_drop_item() -> PackedScene:
+	if drop_items.size() == 1:
+		return drop_items[0]
+	
+	var max_chance: float = 0.0
+	for drop_chance in drop_chances:
+		max_chance += drop_chance
+	
+	var random_value = randf() * max_chance
+	
+	var needle: float = 0.0
+	for i in drop_items.size():
+		var drop_item = drop_items[i]
+		var drop_chance = drop_chances[i] if i < drop_chances.size() else 1
+		if random_value <= drop_chance + needle:
+			return drop_item
+		needle += drop_chance
+		
+	return drop_items[0]
--- systems/mob_spawner.gd
+++ systems/mob_spawner.gd
@@ -17,11 +17,21 @@ func _process(delta):
 	var interval = 60.0 / mobs_per_minute
 	cooldown = interval
 	
+	#validar se ponto é válido
+	var point = get_point()
+	var world_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = point
+	parameters.collision_mask = 0b1001
+	var result: Array = world_state.intersect_point(parameters, 1)
+	if !result.is_empty():
+		return
+	
 	#instanciar criatura
 	var index = randi_range(0, creatures.size() - 1)
 	var creature_scene = creatures[index]
 	var creature = creature_scene.instantiate()
-	creature.global_position = get_point()
+	creature.global_position = point
 	get_parent().add_child(creature)
 
 func get_point() -> Vector2:
